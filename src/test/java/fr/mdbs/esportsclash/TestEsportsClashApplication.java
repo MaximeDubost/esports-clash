232748dd4a98642a8c211b9416219d71564e7c3e
Maxime <maxime.dubost.75@gmail.com>
2025-06-04 11:30:06 +0200

    build(): project initialization

---
+++ src/test/java/fr/mdbs/esportsclash/TestEsportsClashApplication.java
@@ -0,0 +1,11 @@
+package fr.mdbs.esportsclash;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestEsportsClashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(EsportsClashApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
